55ac9a01e28a34b3e134a6092503b6495662ef06
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-22 15:28:31 -0500

    [add] :: provider movies implementation  ✅

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,26 @@
+import 'package:cinema_movie/domain/entities/movie.dart';
+import 'package:cinema_movie/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MoviesNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+  return MoviesNotifier(
+    fetchMoreMovies: fetchMoreMovies,
+  );
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({required this.fetchMoreMovies}) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+    final List<Movie> newMovies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...newMovies];
+  }
+}
