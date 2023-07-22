fbaa5bc682e663b3e5f27916c2bee74acc3c422d
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-22 14:33:19 -0500

    [add] :: repository implementation, c2

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinema_movie/domain/datasources/movies_datasource.dart';
+import 'package:cinema_movie/domain/entities/movie.dart';
+import 'package:cinema_movie/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDataSource innDataSource;
+  MovieRepositoryImpl(this.innDataSource); // Constructor injection
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return innDataSource.getNowPlaying(page: page);
+  }
+}
