7eeb4dc1a1072284b4eca1acaafb8d3ede933da9
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-24 00:49:24 -0500

    [add] :: Slides + pagination

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -10,6 +10,8 @@ class MoviesSlideshow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return SizedBox(
       height: 210,
       width: double.infinity,
@@ -18,6 +20,12 @@ class MoviesSlideshow extends StatelessWidget {
           scale: 0.9,
           autoplay: true,
           itemCount: movies.length,
+          pagination: SwiperPagination(
+              margin: const EdgeInsets.only(top: 0),
+              builder: DotSwiperPaginationBuilder(
+                  // activeSize: 12, space: 5,
+                  activeColor: colors.primary,
+                  color: colors.secondary)),
           itemBuilder: (context, index) => _Slide(movie: movies[index])),
     );
   }
