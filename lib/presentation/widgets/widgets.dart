b419bcd9aeafb2166f2008515eb4aa04abab591d
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-24 19:42:09 -0500

    [add] (footer) v1

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'package:cinema_movie/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:cinema_movie/presentation/widgets/shared/custom_appbar.dart';
+export 'shared/custom_bottom_navigation.dart';
