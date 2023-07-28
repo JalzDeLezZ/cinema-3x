142e5cc0865e0291d7767c7836d6d395d0120117
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-07-27 22:51:33 -0500

    [imp 3] (create PROVIDER  & consume ):: Render Screen 1.1 ✅

--- lib/infrastructure/models/moviedb/movie_details.dart
+++ lib/infrastructure/models/moviedb/movie_details.dart
@@ -114,20 +114,20 @@ class BelongsToCollection {
     final int id;
     final String name;
     final String posterPath;
-    final String backdropPath;
+    final String? backdropPath;
 
     BelongsToCollection({
         required this.id,
         required this.name,
         required this.posterPath,
-        required this.backdropPath,
+        this.backdropPath,
     });
 
     factory BelongsToCollection.fromJson(Map<String, dynamic> json) => BelongsToCollection(
         id: json["id"],
         name: json["name"],
         posterPath: json["poster_path"],
-        backdropPath: json["backdrop_path"],
+        backdropPath: json["backdrop_path"] ?? '',
     );
 
     Map<String, dynamic> toJson() => {
@@ -160,13 +160,13 @@ class Genre {
 
 class ProductionCompany {
     final int id;
-    final String logoPath;
+    final String? logoPath;
     final String name;
     final String originCountry;
 
     ProductionCompany({
         required this.id,
-        required this.logoPath,
+        this.logoPath,
         required this.name,
         required this.originCountry,
     });
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,17 +1,155 @@
+import 'package:cinema_movie/domain/entities/movie.dart';
+import 'package:cinema_movie/presentation/providers/movies/movie_detail_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
   final String movieId;
 
   const MovieScreen({super.key, required this.movieId});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
+
+    if (movie == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(strokeWidth: 2),
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Movie $movieId'),
-      ),
+        body: CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        _CustomSliverAppBar(movie: movie),
+        SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _MovieDetails(movie: movie),
+                childCount: 1))
+      ],
+    ));
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliverAppBar({
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+        backgroundColor: Colors.black,
+        expandedHeight: size.height * 0.7,
+        foregroundColor: Colors.white,
+        titleSpacing: 0,
+        flexibleSpace: FlexibleSpaceBar(
+          titlePadding:
+              const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          centerTitle: false,
+          title: Text(
+            movie.title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.start,
+          ),
+          background: Stack(children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox.expand(
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            stops: [0.6, 1],
+                            colors: [Colors.transparent, Color(0xBB000000)])))),
+            const SizedBox.expand(
+                child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            stops: [0, 0.3],
+                            colors: [Color(0x8D000000), Colors.transparent])))),
+          ]),
+        ));
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+  final Movie movie;
+  const _MovieDetails({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              //! Poster
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(8)),
+                child: Image.network(movie.posterPath,
+                    width: size.width * 0.3, fit: BoxFit.cover),
+              ),
+              const SizedBox(width: 10),
+
+              //! Description
+              SizedBox(
+                  width: (size.width - 40) * 0.7,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(movie.title, style: textStyle.titleLarge),
+                        Text(movie.overview, style: textStyle.bodySmall),
+                      ]))
+            ])),
+
+        //! Genres
+        Padding(
+            padding: const EdgeInsets.all(8),
+            child: Wrap(
+              spacing: 10, // spacing horizontally between chips
+              runSpacing: 10, // spacing vertically between chips
+              children: movie.genreIds
+                  .map((e) => Chip(
+                      label: Text(e),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20))))
+                  .toList(),
+            )),
+
+        const SizedBox(height: 60),
+      ],
     );
   }
 }
