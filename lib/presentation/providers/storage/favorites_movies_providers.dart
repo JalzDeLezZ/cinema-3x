0661642bd5ed9b0d5e41f7b1b96ec56bce05e3f1
James Jalz Díaz López <j4mes.delez@gmail.com>
2023-08-03 16:20:23 -0500

    [imp] (Widg_Favorit) :: Render List Movies 1.1

---
+++ lib/presentation/providers/storage/favorites_movies_providers.dart
@@ -0,0 +1,29 @@
+import 'package:cinema_movie/domain/entities/movie.dart';
+import 'package:cinema_movie/domain/repositories/local_storage_repository.dart';
+import 'package:cinema_movie/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final favoriteMoviesProvider =
+    StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+  final localStorageRepository = ref.watch(localStorageRepositoryProvider);
+  return StorageMoviesNotifier(localStorageRepository: localStorageRepository);
+});
+
+class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
+  int page = 0;
+  final LocalStorageRepository localStorageRepository;
+
+  StorageMoviesNotifier({required this.localStorageRepository}) : super({});
+
+  Future<void> loadNextPage() async {
+    final movies = await localStorageRepository.loadMovies(offset: page * 10);
+    page++;
+
+    final tempMoviesMap = <int, Movie>{};
+    for (final movie in movies) {
+      tempMoviesMap[movie.id] = movie;
+    }
+
+    state = {...state, ...tempMoviesMap};
+  }
+}
--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,10 +1,35 @@
+import 'package:cinema_movie/presentation/providers/storage/favorites_movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: favoriteMovies.length,
+        itemBuilder: (context, index) {
+          final movie = favoriteMovies[index];
+          return ListTile(
+            title: Text(movie.title),
+          );
+        },
+      ),
+    );
   }
 }
